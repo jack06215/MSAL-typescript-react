8ab4f80bded26020fc75e8ac1745b1bfad532613
Jack Cho <jack06215@gmail.com>
2022-05-23 17:59:53 +0800

    setup

--- sample-code/src/apiConfig.ts
+++
@@ -1,5 +0,0 @@
-// The current application coordinates were pre-registered in a B2C tenant.
-export const apiConfig = {
-    b2cScopes: ["https://jackazureadb2c.onmicrosoft.com/tasks-api/tasks.read"],
-    webApi: "http://localhost:5000/hello",
-};
--- sample-code/src/ui-components/Loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Typography } from "@material-ui/core";
-
-export const Loading = () => {
-    return <Typography variant="h6">Authentication in progress...</Typography>
-}
